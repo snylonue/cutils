ecf88f8e7282eae39162b5f3721c84d310436e9f
snylonue <snylonue@gmail.com>
2023-10-10 20:31:37 +0800

    format code

--- math/prime.c
+++ math/prime.c
@@ -4,7 +4,6 @@
 #include <stddef.h>
 #include <stdlib.h>
 
-
 struct vec *primes(int n) {
   struct vec *prime = vec_create(sizeof(int));
 
@@ -20,15 +19,15 @@ struct vec *primes(int n) {
       vec_push(prime, &i);
     }
     for (size_t j = 0; j < prime->len; j += 1) {
-        int p = *(int*)vec_get(prime, j);
-        if (i > n / p) {
-          break;
-        }
-        vis[i * p - 1] = true;
-        if (i % p == 0) {
-          break;
-        }
+      int p = *(int *)vec_get(prime, j);
+      if (i > n / p) {
+        break;
       }
+      vis[i * p - 1] = true;
+      if (i % p == 0) {
+        break;
+      }
+    }
   }
 
   return prime;
